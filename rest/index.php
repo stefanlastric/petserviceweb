ab39b5cbe05f15d68e0a52a3122af92ce3ba9a91
stefanlastric <stefanlastric@gmail.com>
2019-06-12 01:44:54 +0200

    rute fixane

--- rest/index.php
+++ rest/index.php
@@ -18,6 +18,11 @@ Flight::route('GET /pets', function(){
     $pet = Flight::db()->query('SELECT * FROM pets', PDO::FETCH_ASSOC)->fetchAll();
     Flight::json($pet);
 });
+Flight::route('GET /lost_pets', function(){
+    $lostpet = Flight::db()->query('SELECT * FROM lost_pets', PDO::FETCH_ASSOC)->fetchAll();
+    Flight::json($lostpet);
+});
+
 Flight::route('GET /appointments', function(){
     $appointment = Flight::db()->query('SELECT * FROM appointments', PDO::FETCH_ASSOC)->fetchAll();
     Flight::json($appointment);
@@ -38,6 +43,23 @@ Flight::route('POST /pets', function(){
         Flight::json(['message' => "Pet ".$request['Name']." has been added successfully"]);
     }
 });
+
+Flight::route('POST /services', function(){
+    $request = Flight::request()->data->getData();
+    if (isset($request['serviceID']) && $request['serviceID'] !=''){
+        $update = "UPDATE services SET idServices = :idServices, Name = :name, City = :city, Adddress = :address, Type = :type, WorkingTime = :WorkingTime WHERE idServices=:id";
+        $stmt= Flight::db()->prepare($update);
+        $stmt->execute($request);
+        Flight::json(['message' => "Service ".$request['Name']." has been updated successfully"]);
+    }else{
+        unset($request['id']);
+        $insert = "INSERT INTO services (idServices, Name, City, Adddress, Type, WorkingTime) VALUES(:idServices, :Name, :City, :Address, :Type, :WorkingTime)";
+        $stmt= Flight::db()->prepare($insert);
+        $stmt->execute($request);
+        Flight::json(['message' => "Service ".$request['Name']." has been added successfully"]);
+    }
+});
+
 Flight::route('POST /appointments', function(){
     $request = Flight::request()->data->getData();
     if (isset($request['idappointments']) && $request['idappointments'] !=''){
@@ -66,6 +88,19 @@ Flight::route('GET /pets/@id', function($id){
     $pet = $stmt->fetch(PDO::FETCH_ASSOC);
     Flight::json($pet);
 });
+Flight::route('DELETE /lost_pets/@id', function($id){
+    $query = "DELETE FROM lost_pets WHERE idlost_pets = :id";
+    $stmt= Flight::db()->prepare($query);
+    $stmt->execute(['idlost_pets' => $id]);
+    Flight::json(['message' => "Lost pet has been deleted successfully"]);
+});
+Flight::route('GET /lost_pets/@id', function($id){
+    $query = "SELECT * FROM lost_pets WHERE idlost_pets = :id";
+    $stmt= Flight::db()->prepare($query);
+    $stmt->execute(['idlost_pets' => $id]);
+    $lostpet = $stmt->fetch(PDO::FETCH_ASSOC);
+    Flight::json($lostpet);
+});
 
 Flight::route('DELETE /appointments/@id', function($id){
     $query = "DELETE FROM appointments WHERE id = :id";
@@ -93,6 +128,21 @@ Flight::route('GET /services/@id', function($id){
     $stmt->execute(['idServices' => $id]);
     $service = $stmt->fetch(PDO::FETCH_ASSOC);
     Flight::json($service);
+});
+Flight::route('POST /lost_pets', function(){
+    $request = Flight::request()->data->getData();
+    if (isset($request['idlost_pets']) && $request['idlost_pets'] !=''){
+        $update = "UPDATE lost_pets SET idlost_pets= :idlost_pets, Name = :name, Type = :type, City = :city, Contact = :contact, Reward = :reward WHERE id=:id";
+        $stmt= Flight::db()->prepare($update);
+        $stmt->execute($request);
+        Flight::json(['message' => "Lost pet ".$request['Name']." has been updated successfully"]);
+    }else{
+        unset($request['idlost_pets']);
+        $insert = "INSERT INTO lost_pets (idlost_pets, Name, Type, City, Contact, Reward) VALUES( :idlost_pets, :name, :type, :city, :contact, :reward)";
+        $stmt= Flight::db()->prepare($insert);
+        $stmt->execute($request);
+        Flight::json(['message' => "Lost pet ".$request['Name']." has been added successfully"]);
+    }
 });
 Flight::start();
 ?>
